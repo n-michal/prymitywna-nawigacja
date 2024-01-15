9009cda34190e7f6f1422d1febaee848eb21b478
Michal Nowak <michal@e-nowak.pl>
2024-01-15 16:50:57 +0100

    Fix import

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:navigation/permissions_page.dart';
-
-import 'map_page.dart';
+import 'package:navigation/map_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
